d0013f4f4d7350fdc9d37c346e050388a5bd0435
Zach Schneider <zach@schneid.io>
2019-07-23 15:51:24 -0400

    Refactor Todo component to functional style

--- src/Todo.js
+++ src/Todo.js
@@ -1,57 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import classnames from "classnames";
 import { FaRegCheckSquare, FaRegSquare } from "react-icons/fa";
 import { patchJSON } from "./api";
 import "./Todo.css";
 
-export default class Todo extends Component {
-  complete = async () => {
-    const todo = await patchJSON(`/todos/${this.props.todo.id}`, {
-      status: "complete"
+const Todo = ({ todo, users, updateTodo }) => {
+  const assignee = users.find((user) => user.id === todo.userId);
+
+  const updateCheckbox = async (status) => {
+    const updatedTodo = await patchJSON(`/todos/${todo.id}`, {
+      status
     });
-    this.props.updateTodo(todo);
+    updateTodo(updatedTodo);
   };
 
-  uncomplete = async () => {
-    const todo = await patchJSON(`/todos/${this.props.todo.id}`, {
-      status: "incomplete"
-    });
-    this.props.updateTodo(todo);
-  };
+  return (
+    <div className="todo">
+      <h2 className="todo-title">
+        {todo.status === "complete" ? (
+          <FaRegCheckSquare
+            onClick={() => updateCheckbox("incomplete")}
+            role="checkbox"
+            aria-checked="true"
+          />
+        ) : (
+          <FaRegSquare
+            onClick={() => updateCheckbox("complete")}
+            role="checkbox"
+            aria-checked="false"
+          />
+        )}{" "}
+        <span
+          className={classnames({
+            "todo-title-completed": todo.status === "complete"
+          })}
+        >
+          {todo.title}
+        </span>
+      </h2>
+      <p className="todo-assignee">
+        <em>Assigned to:</em> {assignee.name}
+      </p>
+    </div>
+  );
+};
 
-  render() {
-    const { todo, users } = this.props;
-
-    const assignee = users.find((user) => user.id === todo.userId);
-
-    return (
-      <div className="todo">
-        <h2 className="todo-title">
-          {todo.status === "complete" ? (
-            <FaRegCheckSquare
-              onClick={this.uncomplete}
-              role="checkbox"
-              aria-checked="true"
-            />
-          ) : (
-            <FaRegSquare
-              onClick={this.complete}
-              role="checkbox"
-              aria-checked="false"
-            />
-          )}{" "}
-          <span
-            className={classnames({
-              "todo-title-completed": todo.status === "complete"
-            })}
-          >
-            {todo.title}
-          </span>
-        </h2>
-        <p className="todo-assignee">
-          <em>Assigned to:</em> {assignee.name}
-        </p>
-      </div>
-    );
-  }
-}
+export default Todo;
